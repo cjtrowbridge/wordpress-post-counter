787561d5e32f0b98017a1eceffa9b1c2ec5c54eb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-10 08:17:11 -0800

    Update wordpress-post-counter.php

--- wordpress-post-counter.php
+++ wordpress-post-counter.php
@@ -8,8 +8,14 @@
 */
 
 add_shortcode('post-counter', 'wpPostCounter');
+add_shortcode('post-counter-no-attribution', 'wpPostCounterNoAttribution');
 
+function wpPostCounterNoAttribution(){
+	$wpPostCount = $wpdb->get_row( "SELECT count(*) as 'Count' FROM $wpdb->posts", ARRAY_A );
+	echo '<span class="wordpress-post-counter">'.$wpPostCount[0]['Count'].'</span>';
+}
 function wpPostCounter(){
 	$wpPostCount = $wpdb->get_row( "SELECT count(*) as 'Count' FROM $wpdb->posts", ARRAY_A );
-	echo $wpPostCount[0]['Count'];
+	echo '<span class="wordpress-post-counter"><a href="https://github.com/cjtrowbridge/wordpress-post-counter" target="_blank" class="wordpress-post-counter-link">'.$wpPostCount[0]['Count'].'</a></span>';
 }
+
